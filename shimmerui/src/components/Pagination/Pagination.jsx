b192c11a0c5958bc55eb191b6aa6ccf64bf334e2
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2024-05-17 22:09:25 +0530

    pagination

--- shimmerui/src/components/Pagination/Pagination.jsx
+++ shimmerui/src/components/Pagination/Pagination.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+const limit =10;
 const Pagination = () => {
+    const [currentPage,setCurrentPage]=useState(0);
+    useEffect(()=>{
+        fetchProducts();
+    },[])
+    const fetchProducts =async ()=>{
+        
+        const data= await fetch(`https://dummyjson.com/products?limit=${limit}&skip=${currentPage * limit}&select=title,price,description`);
+        const json= await data.json();
+        console.log(json)
+    }
   return (
     <div>Pagination</div>
   )
